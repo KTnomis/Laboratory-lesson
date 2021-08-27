5257720ee20c969e5a04a4157c9256c1d30e25f3
KTnomis <70838982+KTnomis@users.noreply.github.com>
2021-08-27 23:29:50 +0700

    changes
    e

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,16 @@
-let redirect = document.getelementById("click")
-redirect.addEventlistener("click", function(e){
-    window.location.href = ""
-})
+localstorage.setItem("pass", "password")
+localstorage.setItem("account", "dude")
+
+function clickers() {
+    var x = document.getElementById("myPsw").value;
+    var y = document.getElementById("MyAc").value;
+    if ( x == "password" && y =="dude") {
+         windows.location.href=""
+    }
+    else{
+        function nah(){
+            document.getElementByID("nope").innerHTML = "wrong password"
+        }
+    }
+
+}
